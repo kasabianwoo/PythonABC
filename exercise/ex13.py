48899c67d0bfab81122e8bf3c6290e0036dec942
kasabian <kasabianlounge@gmail.com>
2018-02-25 11:35:16 +0800

    update exercise 14

---
+++ exercise/ex13.py
@@ -0,0 +1,39 @@
+# -*- coding:utf-8 -*-
+
+'''
+from sys import argv
+
+script,first,second,third = argv
+
+print "the script is called:",script
+print "your first variable is:",first
+print "your second variable is:",second
+print "your third variable is:",third
+
+'''
+
+'''
+
+from sys import argv
+
+script,first,second = argv
+
+print "the script is called:",script
+print "your first variable is:",first
+print "your second variable is:",second
+
+'''
+
+from sys import argv
+
+script,first,second,third,fourth = argv
+
+city = raw_input("which city you want to travel?")
+print "which city you want to travel? %r " % city
+print "the script is called:",script
+print "your first variable is:",first
+print "your second variable is:",second
+print "your third variable is:",third
+print "your fourth variable is:",fourth
+
+
