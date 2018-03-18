3f4b288d0abade9a884785ef4b0eb8de5022d859
kasabian <kasabianlounge@gmail.com>
2018-03-18 21:51:20 +0800

    update note 23

--- exercise/ex22.py
+++ exercise/ex22.py
@@ -7,6 +7,14 @@ print 'Hello Again'
 test_print = "Hello World!"
 print test_print 
 print "Say:%s" % test_print 
-'''
+
 
 print "Hey %s there." % "you"
+
+'''
+
+
+filename = raw_input(">")
+txt = open(filename,'w')
+txt.truncate()
+
