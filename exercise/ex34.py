9c2fbd7e6f2e976d24c9ce60bd7e1b3fbb98dd62
kasabian <kasabianlounge@gmail.com>
2018-04-03 00:57:33 +0800

    update note 35

---
+++ exercise/ex34.py
@@ -0,0 +1,9 @@
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print " The animal at 0 is the 1st animal and is a  %r " % animals[0]
+print " The animal at 1 is the 2sed animal and is a %r " % animals[1] 
+print " The animal at 2 is the 3rd animal and is a %r " % animals[2]
+print " The animal at 3 is the 4th animal and is a %r " % animals[3]
+print " The animal at 4 is the 5th animal and is a %r " % animals[4]
+print " The animal at 5 is the 6th animal and is a %r " % animals[5]
